79fc98abdab2157a117613bc5fee334dc24d80c4
Farhat Saleem <41129835+fsaleemm@users.noreply.github.com>
2021-02-22 09:14:16 -0500

    updated custom searchers config

--- CredScan/src/CSharpProject/SomeConsoleApplication/Program.cs
+++ CredScan/src/CSharpProject/SomeConsoleApplication/Program.cs
@@ -14,7 +14,7 @@ namespace SomeConsoleApplication
             string myAPIKey1 = "1233-4234-6545-6732-3433";
             string myAPIKey2 = "1b299efe-f902-4f11-aadc-a9a7c7413cf1";
 
-            string mySSN = "044-77-0537";
+            string mySSN = "078-05-1120";
 
             string dbConStr = "Server=tcp:abcd-sql.database.windows.net,1433;Initial Catalog=mydb-dev;Persist Security Info=False;User ID=dummyUser;Password=ThisAreMyCr3denti@ls;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
